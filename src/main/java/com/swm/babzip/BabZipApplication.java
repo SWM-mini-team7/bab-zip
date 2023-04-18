4a0921008a1146491d6a4967c7eb76b31f52affe
Hyeonjun Park <phjppo0918@gmail.com>
2023-04-18 19:28:01 +0900

    feat : init setting

---
+++ src/main/java/com/swm/babzip/BabZipApplication.java
@@ -0,0 +1,13 @@
+package com.swm.babzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabZipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabZipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swm/babzip/BabZipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swm.babzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabZipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
