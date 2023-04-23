6001f9d11f4dfcab9980e9d9be373be7eff49a70
Hyeonjun Park <phjppo0918@gmail.com>
2023-04-23 19:55:07 +0900

    feat : onBoarding connection

---
+++ src/main/java/com/swm/babzip/view/IndexController.java
@@ -0,0 +1,13 @@
+package com.swm.babzip.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String onBoarding() {
+        return "onBoarding";
+    }
+}
